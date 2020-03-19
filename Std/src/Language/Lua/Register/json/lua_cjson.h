fc69c3efb407415ce0f9c9ea52869fc13c1764fb
alittlesail <757609414@qq.com>
2020-03-19 18:16:18 +0800

    提交初始代码

---
+++ Std/src/Language/Lua/Register/json/lua_cjson.h
@@ -0,0 +1,5 @@
+
+#include "LuaJIT-2.1.0-beta3/src/lua.hpp"
+
+extern int luaopen_cjson(lua_State *l);
+extern int luaopen_cjson_safe(lua_State *l);
---
+++ Std/src/Language/Lua/Register/sqlite/lsqlite3.h
@@ -0,0 +1,9 @@
+
+#ifndef lsqlite3_h
+#define lsqlite3_h
+
+#include "LuaJIT-2.1.0-beta3/src/lua.hpp"
+
+int luaopen_lsqlite3(lua_State *L);
+
+#endif // base64_h

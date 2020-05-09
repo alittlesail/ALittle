30522a1322d81281c3bc82518eb48a358a9ef818
xingfanou <757609414@qq.com>
2020-05-09 18:01:01 +0800

    提交

--- ALittle/Module/Emulator/Script/GUtility.lua
+++ ALittle/Module/Emulator/Script/GUtility.lua
@@ -21,69 +21,12 @@ function UtilityCreateTreeMessage(msg, msg_field_name)
 		local field_descriptor = protobuf.messagedescriptor_field(detail_info.info.descriptor, i)
 		local field_name = protobuf.fielddescriptor_name(field_descriptor)
 		if protobuf.fielddescriptor_ismap(field_descriptor) then
-			local msg_count = protobuf.reflection_getrepeatedmessagecount(rflct, msg, field_descriptor)
-			local map_descriptor = protobuf.fielddescriptor_messagetype(field_descriptor)
-			local key_descriptor = protobuf.messagedescriptor_findfieldbyname(map_descriptor, "key")
-			local key_type = protobuf.fielddescriptor_cpptypename(key_descriptor)
-			local value_descriptor = protobuf.messagedescriptor_findfieldbyname(map_descriptor, "value")
-			local value_type = protobuf.fielddescriptor_cpptypename(value_descriptor)
-			local tree_map = IDETreeMap(g_Control, field_name, key_type, value_type)
+			local tree_map = IDETreeMap(g_Control, field_name, rflct, msg, field_descriptor)
 			detail_info.tree:AddChild(tree_map)
-			local index = 0
-			while true do
-				if not(index < msg_count) then break end
-				local sub_msg = protobuf.reflection_getrepeatedmessage(rflct, msg, field_descriptor, index)
-				local sub_rflct = protobuf.message_getreflection(sub_msg)
-				local key_cpp_type = protobuf.fielddescriptor_cpptype(key_descriptor)
-				if key_cpp_type == 7 then
-					local value = protobuf.reflection_getbool(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 1 then
-					local value = protobuf.reflection_getint32(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 3 then
-					local value = protobuf.reflection_getuint32(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 2 then
-					local value = protobuf.reflection_getint64(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 4 then
-					local value = protobuf.reflection_getuint64(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 5 then
-					local value = protobuf.reflection_getdouble(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 6 then
-					local value = protobuf.reflection_getfloat(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 8 then
-					local value = protobuf.reflection_getenumvalue(sub_rflct, sub_msg, key_descriptor)
-				elseif key_cpp_type == 9 then
-					local value = protobuf.reflection_getstring(sub_rflct, sub_msg, key_descriptor)
-				end
-				local value_cpp_type = protobuf.fielddescriptor_cpptype(value_descriptor)
-				if value_cpp_type == 7 then
-					local value = protobuf.reflection_getbool(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 1 then
-					local value = protobuf.reflection_getint32(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 3 then
-					local value = protobuf.reflection_getuint32(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 2 then
-					local value = protobuf.reflection_getint64(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 4 then
-					local value = protobuf.reflection_getuint64(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 5 then
-					local value = protobuf.reflection_getdouble(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 6 then
-					local value = protobuf.reflection_getfloat(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 8 then
-					local value = protobuf.reflection_getenumvalue(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 9 then
-					local value = protobuf.reflection_getstring(sub_rflct, sub_msg, value_descriptor)
-				elseif value_cpp_type == 10 then
-					local value = protobuf.reflection_getmessage(sub_rflct, sub_msg, value_descriptor)
-					detail_info.tree:AddChild(UtilityCreateTreeMessage(value, field_name))
-				end
-				index = index+(1)
-			end
 		elseif protobuf.fielddescriptor_isrepeated(field_descriptor) then
 			local tree_repeated = IDETreeRepeated(g_Control, field_name, rflct, msg, field_descriptor)
 			detail_info.tree:AddChild(tree_repeated)
 		else
-			local value_type = protobuf.fielddescriptor_cpptypename(field_descriptor)
 			local cpp_type = protobuf.fielddescriptor_cpptype(field_descriptor)
 			if cpp_type == 7 then
 				local tree_item = IDETreeBool(g_Control, field_name, rflct, msg, field_descriptor)

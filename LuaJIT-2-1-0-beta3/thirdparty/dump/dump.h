914750e4e91242431914daae707a45cea6f386e6
xingfanou <757609414@qq.com>
2020-05-13 15:22:22 +0800

    删除dump代码

--- LuaJIT-2-1-0-beta3/thirdparty/dump/dump.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef _ALITTLE_DUMP_H_
-#define _ALITTLE_DUMP_H_
-
-void dump_setup(const char* name);
-
-#endif // _ALITTLE_DUMP_H_
--- LuaJIT-2-1-0-beta3/thirdparty/dump/dumplib.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef _ALITTLE_DUMPLIB_H_
-#define _ALITTLE_DUMPLIB_H_
-
-#include "lua.h"
-
-LUALIB_API int luaopen_dump(lua_State* l);
-
-#endif // _ALITTLE_DUMPLIB_H_

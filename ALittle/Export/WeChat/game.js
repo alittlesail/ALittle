38a5461cc12ccecc8262cf6ff00e384a08a088d0
xingfanou <757609414@qq.com>
2020-06-16 14:13:37 +0800

    提交导出模板

--- ALittle/Export/WeChat/game.js
+++ ALittle/Export/WeChat/game.js
@@ -1,18 +1,25 @@
-require('./JSNative/libs/weapp-adapter')
-require('./JSNative/libs/symbol')
-require('./JSNative/Core')
-require('./JSNative/native')
-require('./JSNative/md5.min')
-require('./JSNative/Std')
-require('./JSNative/pixi.min.wx');
-import {install} from './JSNative/unsafe-eval/install';
+require('./Module/abcd@project_name@abcd/JSNative/libs/weapp-adapter')
+require('./Module/abcd@project_name@abcd/JSNative/libs/symbol')
+require('./Module/abcd@project_name@abcd/JSNative/runtime')
+require('./Module/abcd@project_name@abcd/JSNative/Core')
+require('./Module/abcd@project_name@abcd/JSNative/native')
+require('./Module/abcd@project_name@abcd/JSNative/md5.min')
+require('./Module/abcd@project_name@abcd/JSNative/Std')
+
+// 适配
+// if (!window.WebGLRenderingContext)
+//  window.WebGLRenderingContext = canvas.getContext("webgl");
+
+require('./Module/abcd@project_name@abcd/JSNative/pixi.min.wx');
+import {install} from './Module/abcd@project_name@abcd/JSNative/unsafe-eval/install';
 install(PIXI);
-require('./JSNative/pixi-textinput');
+require('./Module/abcd@project_name@abcd/JSNative/pixi-textinput');
 
 // 游戏资源所在的域名，端口，起始路径
-window.alittle_hostname = "127.0.0.1";
-window.alittle_port = 80;
-window.alittle_base_url = "ALittle/";
-require('./JSNative/CEngine')
+window.alittle_hostname = "abcd@res_ip@abcd";
+window.alittle_port = abcd@res_port@abcd;
+window.alittle_base_url = "abcd@res_base_path@abcd";
+require('./Module/abcd@project_name@abcd/JSNative/CEngine1')
+require('./Module/abcd@project_name@abcd/JSNative/CEngine2')
 
 window.__ALITTLEAPI_SetupMainModule("Module/abcd@project_name@abcd/", "abcd@project_name@abcd")

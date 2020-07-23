fa368b5f0799a81bcb853b7622868674a10bc9fc
xingfanou <757609414@qq.com>
2020-07-23 19:41:53 +0800

    优化json输出

--- LuaJIT-2-1-0-beta3/thirdparty/protobuf/protobuf.cpp
+++ LuaJIT-2-1-0-beta3/thirdparty/protobuf/protobuf.cpp
@@ -193,12 +193,14 @@ int protobuf_message_parsefromarray(void* m, void* buffer, int size) {
     return ((google::protobuf::Message*)m)->ParseFromArray(buffer, size) ? 1 : 0;
 }
 
-const char* protobuf_message_jsonencode(void* m) {
+const char* protobuf_message_jsonencode(void* m, int short_mode) {
     static std::string temp;
     temp.clear();
     google::protobuf::util::JsonOptions option;
-    option.preserve_proto_field_names = true;
+    option.add_whitespace = short_mode == 0;
     option.always_print_primitive_fields = true;
+    option.preserve_proto_field_names = true;
+    option.always_print_enums_as_ints = true;
     if (google::protobuf::util::MessageToJsonString(*(const google::protobuf::Message*)m, &temp, option).ok())
         return temp.c_str();
     return nullptr;
--- LuaJIT-2-1-0-beta3/thirdparty/protobuf/protobuflib.c
+++ LuaJIT-2-1-0-beta3/thirdparty/protobuf/protobuflib.c
@@ -508,7 +508,8 @@ static int protobuflib_message_jsonencode(lua_State* L)
 {
     void** m = (void**)lua_touserdata(L, 1);
     luaL_argcheck(L, m != 0, 1, "message object is null");
-    const char* json = protobuf_message_jsonencode(*m);
+    int short_mode = lua_toboolean(L, 2);
+    const char* json = protobuf_message_jsonencode(*m, short_mode);
     if (json) lua_pushstring(L, json);
     else lua_pushnil(L);
     return 1;

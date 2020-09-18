a6791931fca684e3f0169d21208b7e7a93bada26
xingfanou <757609414@qq.com>
2020-09-18 15:17:37 +0800

    添加SoundHelper

---
+++ ALittle/LibClient/Helper/SoundHelper.cpp
@@ -0,0 +1,9 @@
+
+#include "SoundHelper.h"
+
+#include "Carp/carp_mixer.h"
+
+namespace ALittle
+{
+	
+} // ALittle
---
+++ ALittle/LibClient/Helper/SoundHelper.h
@@ -0,0 +1,16 @@
+
+#ifndef _ALITTLE_SOUNDHELPER_H_
+#define _ALITTLE_SOUNDHELPER_H_
+
+namespace ALittle
+{
+class LocalFile;
+
+class SoundHelper
+{
+public:
+};
+
+} // ALittle
+
+#endif // _ALITTLE_SOUNDHELPER_H_

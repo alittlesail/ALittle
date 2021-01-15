b60fc03e9a72fd75c15ced12cf8b95e05858ea8a
xingfanou <757609414@qq.com>
2021-01-15 16:31:50 +0800

    删除模块

--- ALittleIDE/DeplayServer.bat
+++
@@ -1,1 +0,0 @@
-ALittleServer.exe Module/ALittleIDE/Other/GameLibrary/Core/ Module/ALittleIDE/Other/GameLibrary/Std/ Module/ALittleIDE/Other/GameLibrary/SEngine/ DeployServer,DeployServer,Module/ALittleIDE/Other/GameLibrary/DeployServer/,Module/ALittleIDE/Other/GameLibrary/DeployServer.cfg

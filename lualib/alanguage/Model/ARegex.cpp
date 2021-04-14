0f6045204bf1ee1feeb3a29193dadf978523f888
xingfanou <757609414@qq.com>
2021-04-14 16:43:46 +0800

    添加注释

--- lualib/alanguage/Model/ARegex.cpp
+++ lualib/alanguage/Model/ARegex.cpp
@@ -2,75 +2,96 @@
 
 ARegex::ARegex(const std::string& rule) : m_rule(rule)
 {
+    // 清空规则编译错误
     m_rule_error.clear();
-
+    // 根节点是一个OPTION
     size_t offset = 0;
     m_node.type = ARegexNodeType::NT_OPTION;
+    // 开始解析规则串
     CompileOption(m_rule, offset, m_node, m_rule_error);
 }
 
 bool ARegex::Match(const char* src, int& length)
 {
+    // 初始化长度为0
     length = 0;
+    // 进行repeated节点匹配
     return MatchNodeRepeated(src, m_node, length);
 }
 
 bool ARegex::MatchNode(const char* src, const ARegexNode& node, int& length)
 {
+    // 取出当前字符
     const char c = src[length];
+    // 如果已经到结尾了，那么就直接返回
     if (c == 0) return false;
 
+    // 处理当个字符匹配
     if (node.type == ARegexNodeType::NT_CHAR)
     {
+        // 如果字符相等，那么就算是匹配成功
         if (node.value == c)
         {
             ++length;
             return true;
         }
+        // 否则匹配失败
         return false;
     }
 
+    // 处理查找字符串匹配
     if (node.type == ARegexNodeType::NT_FIND_STRING)
     {
         const auto* find = strstr(src + length, node.find.c_str());
+        // 如果查找到，就算是匹配成功
         if (find != nullptr)
         {
             length += static_cast<int>(find - src - length) + static_cast<int>(node.find.length());
             return true;
         }
+        // 否则匹配失败
         return false;
     }
-    
+
+    // 处理字符集合匹配
     if (node.type == ARegexNodeType::NT_CHARSET)
     {
         bool check = false;
+        // 全匹配
         if (node.char_set_type == ARegexCharSetType::CST_ALL)
         {
             check = true;
         }
+        // 自定义匹配
         else if (node.char_set_type == ARegexCharSetType::CST_CUSTOM)
         {
             check = node.char_set.find(c) != node.char_set.end();
         }
+        // 字母匹配
         else if (node.char_set_type == ARegexCharSetType::CST_A)
         {
             check = c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z';
         }
+        // 数字匹配
         else if (node.char_set_type == ARegexCharSetType::CST_D)
         {
             check = c >= '0' && c <= '9';
         }
+        // 字母，数字，下划线匹配
         else if (node.char_set_type == ARegexCharSetType::CST_W)
         {
             check = c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_';
         }
+        // 非换行任意字符匹配
         else if (node.char_set_type == ARegexCharSetType::CST_DOT)
         {
             check = c != '\n';
         }
 
+        // 处理取反
         if (node.negate == ARegexNegateType::NNT_TRUE)
         {
+            // 如果有取反，并且check失败，那么就认为成功
             if (!check)
             {
                 ++length;
@@ -79,6 +100,7 @@ bool ARegex::MatchNode(const char* src, const ARegexNode& node, int& length)
         }
         else
         {
+            // 没有取反，并且check成功才是成功
             if (check)
             {
                 ++length;
@@ -86,89 +108,127 @@ bool ARegex::MatchNode(const char* src, const ARegexNode& node, int& length)
             }
         }
 
+        // 最后是匹配失败
         return false;
     }
-    
+
+    // 处理分支匹配，只要某个子节点匹配成功才是成功
     if (node.type == ARegexNodeType::NT_OPTION)
     {
-        for (auto& child : node.childs)
+        // 遍历所有子节点
+        for (const auto& child : node.childs)
         {
-            // 保存临时值
+            // 保存临时值，每个子节点都是从这个位置开始匹配
             int length_temp = length;
+            // 开始匹配
             if (MatchNodeRepeated(src, child, length_temp))
             {
                 // 如果有取反，那么直接返回失败
                 if (node.negate == ARegexNegateType::NNT_TRUE)
                     return false;
 
+                // 匹配成功了，就停止剩下的子节点匹配，返回成功
                 length = length_temp;
                 return true;
             }
         }
 
+        // 执行到这里，说明匹配失败了
+
+        // 如果有取反，那么就表示匹配成功
         if (node.negate == ARegexNegateType::NNT_TRUE)
         {
             ++length;
             return true;
         }
 
+        // 否则返回匹配失败
         return false;
     }
-    
+
+    // 处理列表匹配，必须全部成功才是成功
     if (node.type == ARegexNodeType::NT_LIST)
     {
-        for (auto& child : node.childs)
+        // 遍历所有子节点
+        for (const auto& child : node.childs)
         {
+            // 如果匹配失败了，就是失败了
             if (!MatchNodeRepeated(src, child, length))
                 return false;
         }
+
+        // 这里不用处理node.negate，因为正则表达式规则上，'!'符号不可能修饰的了LIST
+
+        // 执行到这里，所有全部成功了
         return true;
     }
+
+    // 未知类型的节点
     return false;
 }
 
 bool ARegex::MatchNodeRepeated(const char* src, const ARegexNode& node, int& length)
 {
+    // 处理有且仅有一个，则直接调用MatchNode
     if (node.repeat == ARegexRepeatType::NRT_NONE || node.repeat == ARegexRepeatType::NRT_ONE)
-    {
         return MatchNode(src, node, length);
-    }
-    
+
+    // 处理有一个或者没有
     if (node.repeat == ARegexRepeatType::NRT_ONE_OR_NOT)
     {
+        // 保存当前值
         int length_temp = length;
+        // 如果匹配成功，那么就保存新的值
         if (MatchNode(src, node, length_temp))
             length = length_temp;
+        // 不论成功失败，都认为是成功
         return true;
     }
-    
+
+    // 处理没有或者多个
     if (node.repeat == ARegexRepeatType::NRT_NOT_OR_MORE)
     {
+        // 循环处理
         while (true)
         {
+            // 保存当前值
             int length_temp = length;
+            // 如果匹配成功，就保存新的值
             if (MatchNode(src, node, length_temp))
                 length = length_temp;
+            // 匹配失败就退出
             else
                 break;
         }
+
+        // 不论匹配了多少个，都认为是成功
         return true;
     }
-    
+
+    // 处理至少一个
     if (node.repeat == ARegexRepeatType::NRT_AT_LEAST_ONE)
     {
+        // 处理匹配一个，如果失败了就认为是失败
         if (!MatchNode(src, node, length)) return false;
 
+        // 循环处理
         while (true)
         {
+            // 保存当前值
             int length_temp = length;
+            // 如果匹配成功，就保存新的值
             if (MatchNode(src, node, length_temp))
                 length = length_temp;
+            // 匹配失败就退出
             else
                 break;
         }
+
+        // 否则就是成功
         return true;
     }
+
+    // 未知类型就返回失败
     return false;
 }
 

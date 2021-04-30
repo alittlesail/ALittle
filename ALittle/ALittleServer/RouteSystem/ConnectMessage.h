6346f2e2aaecfb19e33f3635e8052d7e2343b26e
xingfanou <757609414@qq.com>
2021-04-30 12:59:02 +0800

    CarpMessage的宏，把message id作为参数

--- ALittle/ALittleServer/RouteSystem/ConnectMessage.h
+++ ALittle/ALittleServer/RouteSystem/ConnectMessage.h
@@ -8,45 +8,41 @@
 namespace ALittle
 {
 
-enum
-{
-	_QConnectRegister			= 100,
-	_AConnectRegister			= 101,
-	_QConnect2ConnectRPCRead	= 102,
-	_QConnect2ConnectRPCWrite	= 102,
-	_AConnect2ConnectRPCRead	= 103,
-	_AConnect2ConnectRPCWrite	= 103,
-};
-
 // 向连接目标注册自己，告诉目标我是谁
-CARP_MESSAGE_MACRO(QConnectRegister
+CARP_MESSAGE_MACRO(QConnectRegister, 100
 	, ROUTE_ID, route_id);
 
 // 向请求方注册自己，告诉请求方我是谁
-CARP_MESSAGE_MACRO(AConnectRegister
+CARP_MESSAGE_MACRO(AConnectRegister, 101
 	, ROUTE_ID, route_id);
 
 // 请求点对点RPC
 
+// QConnect2ConnectRPCRead 和 QConnect2ConnectRPCWrite 其实是同一个消息
+// 一个用来读的，一个用来写的
+
 // 这个协议用于读取
-CARP_MESSAGE_MACRO(QConnect2ConnectRPCRead
+CARP_MESSAGE_MACRO(QConnect2ConnectRPCRead, 102
     , int, message_id
     , int, message_size);
 // 这个协议用于发送
-CARP_MESSAGE_MACRO(QConnect2ConnectRPCWrite
+CARP_MESSAGE_MACRO(QConnect2ConnectRPCWrite, 102
 	, int, message_id
 	, CarpMessagePtrWapper<CarpMessage>, message_body);
 
 
 // 应答点对点RPC
 
+// AConnect2ConnectRPCRead 和 AConnect2ConnectRPCWrite 其实是同一个消息
+// 一个用来读的，一个用来写的
+
 // 这个协议用于读取
-CARP_MESSAGE_MACRO(AConnect2ConnectRPCRead
+CARP_MESSAGE_MACRO(AConnect2ConnectRPCRead, 103
 	, int, message_id
 	, int, message_size);
 
 // 这个协议用于发送
-CARP_MESSAGE_MACRO(AConnect2ConnectRPCWrite
+CARP_MESSAGE_MACRO(AConnect2ConnectRPCWrite, 103
 	, int, message_id
 	, CarpMessagePtrWapper<CarpMessage>, message_body);
 
--- ALittle/ALittleServer/RouteSystem/RouteMessage.h
+++ ALittle/ALittleServer/RouteSystem/RouteMessage.h
@@ -8,46 +8,39 @@
 namespace ALittle
 {
 
-enum
-{
-	_QSearchRouteInfo			= 200,
-	_ASearchRouteInfo			= 201,
-	_QConnectRouteInfo			= 202,
-	_AConnectRouteInfo			= 203,
-	_ConnectRouteDisconnected	= 204,
-	_ConnectRouteMessageRead	= 205,
-	_ConnectRouteMessageWrite	= 205,
-};
-
 // 请求搜索路径
-CARP_MESSAGE_MACRO(QSearchRouteInfo
+CARP_MESSAGE_MACRO(QSearchRouteInfo, 200
 	, std::list<ROUTE_ID>, route_path);
 	
 // 应答搜索路径
-CARP_MESSAGE_MACRO(ASearchRouteInfo
+CARP_MESSAGE_MACRO(ASearchRouteInfo, 201
 	, std::list<ROUTE_ID>, route_id_list);
 	
 // 请求连接路径
-CARP_MESSAGE_MACRO(QConnectRouteInfo
+CARP_MESSAGE_MACRO(QConnectRouteInfo, 202
 	, int, connect_index
 	, std::vector<ROUTE_ID>, route_path);
 	
 // 应答连接成功
-CARP_MESSAGE_MACRO(AConnectRouteInfo);
+CARP_MESSAGE_MACRO(AConnectRouteInfo, 203);
 
 // 路径连接断开了
-CARP_MESSAGE_MACRO(ConnectRouteDisconnected
+CARP_MESSAGE_MACRO(ConnectRouteDisconnected, 204
 	, CONNECT_KEY, connect_key);
 
+
+// 注意 ConnectRouteMessageWrite 和 ConnectRouteMessageRead 消息ID 故意定成一样的
+// 他们是同一个消息，一个用来写，一个同来读
+
 // 路由机制上的消息包，用于读取
-CARP_MESSAGE_MACRO(ConnectRouteMessageRead
+CARP_MESSAGE_MACRO(ConnectRouteMessageRead, 205
     , CONNECT_KEY, connect_key
     , int, message_id
 	, int, message_rpcid
     , int, message_size);
 
 // 路由机制上的消息包，用于写
-CARP_MESSAGE_MACRO(ConnectRouteMessageWrite
+CARP_MESSAGE_MACRO(ConnectRouteMessageWrite, 205
 	, CONNECT_KEY, connect_key
 	, int, message_id
 	, int, message_rpcid
